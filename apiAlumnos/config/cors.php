777da4ca5b593aff49e494a632b2432f2a133a84
Miguel Ángel García Sánchez <example@gmail.com>
2025-03-03 17:07:30 +0100

    fin

--- apiAlumnos/config/cors.php
+++ apiAlumnos/config/cors.php
@@ -3,8 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173',
-    'http://desplieguedemiaplicacion.duckdns.org'], // Orígenes permitidos (frontend)
+    'allowed_origins' => ['http://desplieguedemiaplicacion.duckdns.org'], // Orígenes permitidos (frontend)
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
